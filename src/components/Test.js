c71b4ba9bc8a498894ea797dd29c75413dc29df4
aslamsk <aslamsk777@gmail.com>
2020-01-04 22:41:20 +0530

    first commit

---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Link } from 'gatsby'
+
+export default class Test extends Component {
+  render() {
+    return (
+      <>
+        <Link to="/blogs">touch me</Link>
+      </>
+    )
+  }
+}
